f3ce8904e0d2a727143c5d7325cec271f2d88829
MelissaLiza <33340802+MelissaLiza@users.noreply.github.com>
2022-11-28 20:44:21 -0500

    avance de usuario

--- Oauth/src/main/java/com/tienda/oauth/clients/UserFeignClient.java
+++ Oauth/src/main/java/com/tienda/oauth/clients/UserFeignClient.java
@@ -1,9 +1,14 @@
 package com.tienda.oauth.clients;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.tienda.commons.commons.entity.User;
 
 @FeignClient(name="service-user")
 public interface UserFeignClient {
-	
+	@GetMapping("/user/search/buscar-username")
+	public User findByUsername(@RequestParam String username);
 
 }
---
+++ Users/src/main/resources/import.sql
@@ -0,0 +1,9 @@
+INSERT INTO usuarios(username,password,firstname,lastname,email,createat,updateat)VALUES('jose','123456','JOSE HERNAN','LIZA GARAVITO','jose@gmail.com',NOW(),NOW());
+INSERT INTO usuarios(username,password,firstname,lastname,email,createat,updateat)VALUES('melissa','654321','MELISSA','LIZA SHERON','melissa@gmail.com',NOW(),NOW());
+
+INSERT INTO 'roles'(description)VALUES ('ROLE_USER');
+INSERT INTO 'roles'(description)VALUES ('ROLE_ADMIN');
+
+INSERT INTO 'usuarios_roles(usuario_id, role_id) VALUES (1,1)';
+INSERT INTO 'usuarios_roles(usuario_id, role_id) VALUES (2,2)';
+INSERT INTO 'usuarios_roles(usuario_id, role_id) VALUES (2,1)';
