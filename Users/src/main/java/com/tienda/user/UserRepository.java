0aa2f93aaeca6af77c9f997b54ebeace5eca34b4
Jose liza <joselizagaravito@gmail.com>
2022-11-28 20:40:09 -0500

    Correccion de data

--- Users/src/main/java/com/tienda/user/UserRepository.java
+++ Users/src/main/java/com/tienda/user/UserRepository.java
@@ -4,14 +4,12 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
-import com.tienda.commons.commons.entity.User;
-
 
 @RepositoryRestResource(path = "user")
 public interface UserRepository extends PagingAndSortingRepository<User, Long> {
-	
-public User findByFirstname(String firstname );
 
-@RestResource(path="buscar-username")
-public User findByUsername(@Param("username")String username );
+	public User findByFirstname(String firstname);
+
+	@RestResource(path = "buscar-username")
+	public User findByUsername(@Param("username") String username);
 }
