32ff9b57c16aee4d01d6f1225ae6b4a9a29caa00
Jose liza <joselizagaravito@gmail.com>
2022-11-29 01:26:50 -0500

    Creacion de Oauth2

--- Oauth/src/main/java/com/tienda/oauth/clients/UserFeignClient.java
+++ Oauth/src/main/java/com/tienda/oauth/clients/UserFeignClient.java
@@ -4,7 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.tienda.commons.commons.entity.User;
+import com.commons.entity.User;
+
 
 @FeignClient(name="service-user")
 public interface UserFeignClient {
--- Oauth/src/main/java/com/tienda/oauth/service/UsuarioService.java
+++ Oauth/src/main/java/com/tienda/oauth/service/UsuarioService.java
@@ -1,15 +1,70 @@
 package com.tienda.oauth.service;
 
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authorization.AuthorityAuthorizationDecision;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import com.commons.entity.User;
+import com.tienda.oauth.clients.UserFeignClient;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
 public class UsuarioService implements UserDetailsService {
+
+	@Autowired
+	private UserFeignClient client;
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+		User usuario = client.findByUsername(username);
+
+		if (usuario == null) {
+			log.error("Error en el login, no existe el usuario'" + username + "' en el sistema");
+			throw new UsernameNotFoundException(
+					"Error en el login, no existe el usuario'" + username + "' en el sistema");
+		}
+
+		List<GrantedAuthority> authorities = usuario.getRoles().stream()
+				.map(role -> new SimpleGrantedAuthority(role.getDescription()))
+				.peek(a -> log.info("Role: " + a.getAuthority())).collect(Collectors.toList());
+		log.info("Usuario autenticado: " + username);
+
+		return new org.springframework.security.core.userdetails.User(usuario.getUsername(), usuario.getPassword(),
+				true, true, true, true, authorities);
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
