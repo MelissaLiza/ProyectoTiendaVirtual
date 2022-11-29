ed1e374e60bde2f0db67d465320ed01e161c59d0
Jose liza <joselizagaravito@gmail.com>
2022-11-28 19:18:31 -0500

    actualizacion de datos de inicio

--- Users/src/main/java/com/tienda/user/UserRepository.java
+++ Users/src/main/java/com/tienda/user/UserRepository.java
@@ -9,7 +9,8 @@ import org.springframework.data.rest.core.annotation.RestResource;
 @RepositoryRestResource(path = "user")
 public interface UserRepository extends PagingAndSortingRepository<User, Long> {
 	
-	
+public User findByFirstname(String firstname );
+
 @RestResource(path="buscar-username")
 public User findByUsername(@Param("username")String username );
 }
--- Users/src/main/resources/insert.sql
+++ Users/src/main/resources/insert.sql
@@ -1,5 +1,6 @@
-INSERT INTO usuarios(username,password,enabled,nombre,apellido,email,createat,updateat)VALUES('jose','123456',TRUE,'JOSE HERNAN','LIZA GARAVITO','joselizag@gmail.com',NOW(),NOW());
-INSERT INTO usuarios(username,password,enabled,nombre,apellido,email,createat,updateat)VALUES('melissa','654321',TRUE,'MELISSA','LIZA SHERON','melissa@gmail.com',NOW(),NOW());
+INSERT INTO usuarios(username,password,firstname,lastname,email,createat,updateat)VALUES('jose','123456','JOSE HERNAN','LIZA GARAVITO','jose@gmail.com',NOW(),NOW());
+INSERT INTO usuarios(username,password,firstname,lastname,email,createat,updateat)VALUES
+('melissa','654321','MELISSA','LIZA SHERON','melissa@gmail.com',NOW(),NOW());
 
 INSERT INTO 'roles'(nombre)VALUES ('ROLE_USER');
 INSERT INTO 'roles'(nombre)VALUES ('ROLE_ADMIN');
